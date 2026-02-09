8082325343e6a87ae373f90708b32a70e4ca6fe4
andriajankara7-star <andria.jankara7@gmail.com>
2026-02-09 12:55:01 +0400

    level 02

---
+++ level 02/classwork/cw.py
@@ -0,0 +1,16 @@
+print("coconut")
+print("tree")
+print("blue")
+print(6)
+print(8)
+print(34)
+print(9.9)
+print(3.4)
+print(6.5)
+
+a = 5
+print(a)
+andro = 37
+print(andro)
+jail = "school"
+print(jail)
---
+++ level 02/homework/hw.py
@@ -0,0 +1,35 @@
+jail = "school"
+aura = "chad"
+goa = "aurafarm"
+programmer = "andro"
+homework = "10 hours wasted on random knowledge"
+
+andro = 9
+birthday = 37
+grade = 4
+fav_number = 67
+year = 2026
+
+a = 67.67
+b = 61.61
+c = 0.67
+d = 0.61
+e = 67.61
+
+print(jail)
+print(aura)
+print(goa)
+print(programmer)
+print(homework)
+
+print(andro)
+print(birthday)
+print(grade)
+print(fav_number)
+print(year)
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
