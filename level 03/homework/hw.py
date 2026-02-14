ea16d11ae958eec72294ba698fd2b0cf5e523c00
andriajankara7-star <andria.jankara7@gmail.com>
2026-02-14 18:42:36 +0400

    level 03 commit

---
+++ level 03/homework/hw.py
@@ -0,0 +1,19 @@
+num1 = 60
+num2 = 30
+print(num1+num2)
+print(num1-num2)
+print(num1/num2)
+print(num1*num2)
+num3 = 70
+num4 = 35
+print(num3+num4)
+print(num3-num4)
+print(num3*num4)
+print(num3/num4)
+num = 67
+name = 61
+print(num*name)
+andro = 334343434
+num67 = 3454546
+num61 = 745483
+print(andro*num67*num61)
