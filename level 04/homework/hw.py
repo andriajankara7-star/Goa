d6338fd8407016dbaccba534de37a7eee1d31b56
andriajankara7-star <andria.jankara7@gmail.com>
2026-02-16 16:32:58 +0400

    level 04 commit

---
+++ level 04/homework/hw.py
@@ -0,0 +1,29 @@
+print("GOA BEST!")
+print("GOA BEST!")
+print("GOA BEST!")
+print("GOA BEST!")
+print("GOA BEST!")
+print("GOA BEST!")
+print("GOA BEST!")
+print("GOA BEST!")
+print("GOA BEST!")
+print("GOA BEST!")
+print("GOA BEST!")
+print("GOA BEST!")
+print("GOA BEST!")
+print("GOA BEST!")
+print("GOA BEST!")
+
+#ცვლადი
+num1= 67
+num2= 66
+num3 = 65
+num4= 64
+num5=63
+print(num1)
+print(num2)
+print(num3)
+print(num4)
+print(num5)
+
+
