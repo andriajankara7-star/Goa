bd410041c50313d218dd98d4be42026bd5d8d46c
andriajankara7-star <andria.jankara7@gmail.com>
2026-02-23 18:43:42 +0400

    level 06 commit

---
+++ level 06/classwork/cw.py
@@ -0,0 +1,4 @@
+i_love_pancakes = 9
+i_lOve_pancakes = 8
+print(i_love_pancakes)
+print(i_lOve_pancakes)
---
+++ level 06/homework/hw.py
@@ -0,0 +1,6 @@
+i_love_turtles = 24
+i_lOve_turtles = 24
+I_Love_Turtles = 24
+print(i_love_turtles)
+print(i_lOve_turtles)
+print(I_Love_Turtles)
