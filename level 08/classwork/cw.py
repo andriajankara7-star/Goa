5e5d38802cc2798b3d277e5ae734bb6abba08653
andriajankara7-star <andria.jankara7@gmail.com>
2026-03-02 18:27:33 +0400

    level 08 commit

---
+++ level 08/classwork/cw.py
@@ -0,0 +1,4 @@
+#1. snake case არის _ მითი შეგვიძლია შევქმნათ გამოტოვებული სიტყვა ერორების გარეშე მაგ: im_hungry
+#2. ქეის სენსიტიურობა არის დიდი და პატარა ასო
+name = input("enter your name:")
+age = input("enter your age:")
---
+++ level 08/homework/hw.py
@@ -0,0 +1,8 @@
+#input-ი ტერმინალიდან შეგვაქვს ინფორმაცია
+name = input("enter your name:")
+print(name)
+last_name = input("enter your last name:")
+print(last_name)
+age = input("enter your age:")
+#input-ი ნიშნავს შეტანას output-გამოტანას
+#იმიტომ რომ ვისუალ სტუდიო კოდი მიხვდეს რომ პიტნოია
