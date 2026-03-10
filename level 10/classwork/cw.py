5e63deb1bced5e14201bc14113006cae88e8d86e
andriajankara7-star <andria.jankara7@gmail.com>
2026-03-10 19:50:56 +0400

    level 11 commit

---
+++ level 10/classwork/cw.py
@@ -0,0 +1,7 @@
+#1)კონკადინაცია არის სტრინგების შეერთება 2)type გვეხმარება გავიგოთ მონაცემთა ტიპი
+anything =input("enter anything:")
+print(type(anything))
+
+name1 = input("enter your name:")
+name2 = input("enter your name:")
+print(name1 + name2)
---
+++ level 10/classwork/homework/hw.py
@@ -0,0 +1,9 @@
+name1 = input("enter your name:")
+print("hello " +  name1)
+#input output არის შეტანა და გამოტანა type გვეხმარება გავიგოთ რა მონაცემთა ტიპია int() str() float() გადააკეთებს ნებისმიერ რამეს int str ან float-ად
+name6 = input("your name is:")
+name4 = input("your age is:")
+name3 = input("your surname is:")
+name5 = (int(input("enter a number:")))
+name2 = (int(input("enter a number:")))
+print(name5 * name2)
