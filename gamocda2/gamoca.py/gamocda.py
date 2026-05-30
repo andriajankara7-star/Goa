1d850ce21d59e3a3c621c2bb1cfb158d4ccd5082
andriajankara7-star <andria.jankara7@gmail.com>
2026-05-30 18:08:49 +0400

    level 31 commit

---
+++ gamocda2/gamoca.py/gamocda.py
@@ -0,0 +1,12 @@
+num = 101
+while num < 500:
+    print(num)
+    num = num + 2
+
+age = int(input("enter your age: "))
+for i in range(5):
+    print(age)
+if age == 18:
+    print("you are 18")
+else:
+    print("you are not 18")
