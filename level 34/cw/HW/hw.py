670cabb25d6c7acdd8670d7ee1a001a304bde0e2
andriajankara7-star <andria.jankara7@gmail.com>
2026-06-06 17:41:32 +0400

    level 35 commit

---
+++ level 34/cw/HW/hw.py
@@ -0,0 +1,8 @@
+sales = [45, 50, 62, 40, 55, 90, 105]
+print(sales[0])
+print(sales[2])
+print(sales[-1])
+sales[3] = 48
+print(sales)
+# list index sclicing არის ლისტის გაჭრა ის გვეხმარება რომ ზოგი რიცხვები რომ ამოვიღოთ მაგ:
+print(sales[2:5])
