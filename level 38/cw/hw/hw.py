7fd0bc2417292b5f3164a640881f34299e064d5c
andriajankara7-star <andria.jankara7@gmail.com>
2026-06-15 18:31:13 +0400

    level 38 commit

---
+++ level 38/cw/hw/hw.py
@@ -0,0 +1,14 @@
+random_numbers = [42, 7, 19, 88, 3, 55, 21, 90, 11, 64]
+print(random_numbers[4])
+mixed_numbers = [-10, 25, -3, 0, 14, -8, 7, -1]
+print(mixed_numbers[1])
+word = str(input("enter a word:"))
+print(len(word))
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+sum = 0
+l = len(numbers)
+for i in range(l):
+    sum = sum + numbers[i]
+print(sum)
+mixed_list = [156, 4, 89, 23, 412, 77, 9, 105, 56, 31]
+print(len(mixed_list))
